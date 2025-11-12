f41aa87e57faf20f3a3d20413dfc39d44ebc3ecb
tossy0130 <34308569+tossy0130@users.noreply.github.com>
2025-11-13 07:59:15 +0900

    add_25_1113_001

---
+++ 確率期待値とアルゴリズム/kokugo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int N, P[59], Q[59];
+double Answer = 0.0;
+
+int main(){
+    // 入力
+    cin >> N;
+    for(int i = 1; i <= N; i++) cin >> P[i] >> Q[i];
+
+    // 答えの計算 => 答えの出力
+    for(int i = 1; i <= N; i++) {
+        Answer += 1.0 * Q[i] / P[i];
+    }
+
+    printf("%.12lf\n", Answer);
+    return 0;
+}
